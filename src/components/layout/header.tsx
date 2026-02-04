93ef211143f89618e0ba5ef0d17edcda811d7400
Isaac Jara <jara.isaac.c@gmail.com>
2026-02-04 14:12:23 +0000

    veo errores

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
 
   return (
     <header className="dark sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-20 items-center">
         <div className="mr-4 flex">
           <Link href="/" className="flex items-center space-x-2">
             <GardenBrosLogo />
