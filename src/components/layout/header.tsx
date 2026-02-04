cc9265f6730dcb6af56714a11606c878d487469e
Isaac Jara <jara.isaac.c@gmail.com>
2026-02-04 16:14:35 +0000

    no quedaron los textos del nav centrados en el header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -21,14 +21,17 @@ export default function Header() {
 
   return (
     <header className="dark sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-20 md:h-24 items-center">
-        <div className="flex-1 md:flex-none">
+      <div className="container flex h-20 md:h-24 items-center justify-between">
+        
+        {/* Left side */}
+        <div className="md:flex-1">
           <Link href="/" className="flex items-center space-x-2">
             <GardenBrosLogo />
           </Link>
         </div>
 
-        <nav className="hidden md:flex flex-1 justify-center items-center space-x-6 text-sm font-medium">
+        {/* Center Nav */}
+        <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
           {navLinks.map((link) => (
             <Link
               key={link.href}
@@ -43,7 +46,8 @@ export default function Header() {
           ))}
         </nav>
 
-        <div className="flex flex-1 justify-end">
+        {/* Right side */}
+        <div className="flex justify-end md:flex-1">
           <div className="md:hidden">
             <Sheet>
               <SheetTrigger asChild>
